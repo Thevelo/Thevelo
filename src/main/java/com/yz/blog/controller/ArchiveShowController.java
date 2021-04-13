63a145ab9d4aa7b4ad3583cb2ec1465b49221163
Thevelo <503184429@qq.com>
2021-04-13 23:32:37 +0800

    aaaaa

--- src/main/java/com/yz/blog/controller/ArchiveShowController.java
+++ src/main/java/com/yz/blog/controller/ArchiveShowController.java
@@ -14,9 +14,9 @@ public class ArchiveShowController {
 
     @GetMapping("/archives")
     public String archives(Model model){
-
+        System.out.println("hello world");
         model.addAttribute("archiveMap",blogService.archiveBlog());
-        model.addAttribute("blogNum",blogService.selectBlogNum());
+        model.addAttribute("blgNum",blogService.selectBlogNum());
         return "archives";
     }
 }
