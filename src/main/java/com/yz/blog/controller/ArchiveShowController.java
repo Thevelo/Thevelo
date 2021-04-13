129704e05a7ca2b2e1a3ee683c092cca8f0e8b67
Thevelo <503184429@qq.com>
2021-04-13 23:50:22 +0800

    d1

--- src/main/java/com/yz/blog/controller/ArchiveShowController.java
+++ src/main/java/com/yz/blog/controller/ArchiveShowController.java
@@ -15,7 +15,7 @@ public class ArchiveShowController {
     @GetMapping("/archives")
     public String archives(Model model){
         System.out.println(123);
-        model.addAttribute("asd",blogService.archiveBlog());
+        model.addAttribute("a123sd",blogService.archiveBlog());
         model.addAttribute("123",blogService.selectBlogNum());
         System.out.println("hello world");
         model.addAttribute("123",blogService.archiveBlog());
